5b39f32152c106a98d7b0e109df3308758b9f7d8
Matteo Figus <matteofigus@gmail.com>
2016-11-12 00:28:39 +0000

    Bugfix

--- cli.js
+++ cli.js
@@ -4,15 +4,16 @@
 
 const fs = require('fs');
 const Mocha = require('mocha');
+const path = require('path');
 
 const registryTests = new Mocha({
 	timeout: 60000,
 	reporter: 'spec'
 });
 
-fs.readdirSync('./test/')
+fs.readdirSync(path.join(__dirname, './test/'))
 	.filter(file => file.substr(-3) === '.js')
-	.forEach(file => registryTests.addFile(`./test/${file}`));
+	.forEach(file => registryTests.addFile(path.join(__dirname, `./test/${file}`)));
 
 registryTests.run(() => {
 	process.exit(0);
